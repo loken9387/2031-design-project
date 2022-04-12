462a4f90b6ebb1f5085b8d06d9d8127951f68b32
loken9387 <84040228+loken9387@users.noreply.github.com>
2022-04-12 18:47:14 -0400

    Add files via upload

--- PixelTest.asm
+++ PixelTest.asm
@@ -3,7 +3,12 @@
 ORG 0
 loop:    LOADI  1
 		 OUT    Poop
-		 jump   loop
+		
+		 LOADI Test_addr
+		 OUT C_ADDR
+		 LOADI Test_color
+		 OUT C_COLOR
+		 
 ; IO address constants
 Switches:  EQU 000
 LEDs:      EQU 001
@@ -13,3 +18,8 @@ Hex1:      EQU 005
 PXL_A:     EQU &H0B0
 PXL_D:     EQU &H0B1
 Poop:	   EQU &H0B2
+C_ADDR:	   EQU &H0B3
+C_COLOR:   EQU &H0B4
+
+Test_addr:  DW  &B0000001
+Test_color: DW  &B0011100011100111
